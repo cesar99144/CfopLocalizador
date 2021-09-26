c7299d13646fe14493a09cbf19e4d68735bc996a
César Ferreira <cesarferreiraa67@gmail.com>
2021-09-26 16:42:47 -0300

    Importação do arquivo de rotas

--- App.js
+++ App.js
@@ -2,39 +2,12 @@ import React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
-
-import Home from './src/pages/CFOP/Home';
-import Devolucao from './src/pages/CFOP/Devolucao';
-
+import Routes from './src/routes';
 
 export default function App(){
   return(
     <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-          name="home" 
-          component={Home}
-          options={{
-            headerStyle:{
-             
-            },
-            headerShown: false
-          }}
-          >
-        </Stack.Screen>
-          <Stack.Screen name="devolucao" component={Devolucao}
-            options={{
-              headerStyle:{
-                backgroundColor: '#34ace0'
-              },
-              headerShown: false
-            }}
-          
-          >
-            
-          </Stack.Screen>
-        </Stack.Navigator>
+          <Routes />
     </NavigationContainer>
   );
 }
